5d332f0a9566eacaaf5c2b3bb460c0e253071e7a
Monseg <mons@mons.ws>
2020-07-29 09:06:08 +0300

    Update search.php

--- montgo/search.php
+++ montgo/search.php
@@ -9,9 +9,9 @@
 	<div id="post-<?php the_ID()?>" <?php post_class( array( 'post-class-v') ) ?>>
 		<span class="cat"><?php the_category(' '); ?></span>  
 		<?php the_content('Читать далее &raquo;'); ?>
-	        <p class="article_tags"><?php if ( function_exists('wp_tag_cloud') ) : ?>
-			 <?php the_tags('', '' , ''); ?>
-			 <?php endif; ?></p>
+	       <p class="article_tags"><?php if ( function_exists('wp_tag_cloud') ) : ?>
+							         <?php the_tags( '<ul class="tags"><li>','</li><li>','</li></ul>'); ?>
+							          <?php endif; ?></p><div style="clear:both"></div>
 			 <span class="post-date float_left">
 			 <?php echo getPostViews(get_the_ID()); ?> |
 			 <?php the_time('j M Y'); ?></span>	
